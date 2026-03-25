1a37e762f4177f4a8d5fd5dab99569e40b06a9bd
lotharsky <hennelothar@gmail.com>
2026-03-25 21:07:41 +0100

    code hover and dynamic grid creation

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 const container = document.createElement("div")
-container.setAttribute("style", 
-    `width: 920px;
-    height: 800px;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    `)
+container.classList.add("container")
+
+
+// setAttribute("style", 
+//     `width: 1000px;
+//     height: 1000px;
+//     border: 10px, solid, black;
+//     display: flex;
+//     justify-content: space-evenly;
+//     flex-wrap: wrap;
+
+//     `)
+
+
 document.body.append(container)
 
+document.body.setAttribute("style", "display: flex; align-items: center; justify-content: center")
 
-for (let i = 0; i < 16; i++) {
-    const box = document.createElement("div")
-    box.setAttribute("style", "width: 200px; height: 200px; background: red; margin: 1rem auto 1rem auto ")
-    container.appendChild(box)
+function createCanvas(nBlocks) {
+
+    const blockSize = (1000 - (16*(nBlocks+1)))/ (nBlocks)
+
+    for (let i = 1; i <= nBlocks*nBlocks && i <= 100; i++) {
+
+        console.log("i: ", i)
+
+        const box = document.createElement("div")
+        box.setAttribute("style", `width: ${blockSize}px; height: ${blockSize}px;`)
+        box.classList.add("box")
+        container.appendChild(box)
+
+        box.addEventListener("mouseover", event => {
+            
+            console.log(event.currentTarget)
+
+            event.currentTarget.classList.add("hover")
+
+            //event.currentTarget.style.background = "black"
+        })
+    }
 }
 
+
+createCanvas(3)
 
 
 
